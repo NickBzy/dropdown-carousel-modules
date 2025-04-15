52da474e275f76a36651d245efe09871c166b829
Nicolas B <97320296+NickBzy@users.noreply.github.com>
2025-04-15 14:31:26 -0400

    Add dropdown menu code

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,1 +1,11 @@
 import "../styles/styles.css"
+
+const dropdownBtn = document.querySelector(".dropdown-button")
+const dropdownMenu = document.querySelector(".dropdown-menu")
+dropdownBtn.addEventListener("click", function () {
+  if (dropdownMenu.classList.contains("open")) {
+    dropdownMenu.classList.remove("open")
+  } else {
+    dropdownMenu.classList.add("open")
+  }
+})
