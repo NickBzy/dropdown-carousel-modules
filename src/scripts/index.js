5fa9b8842f2cd68431e3f8501c8ac6a15fd25af7
Nicolas B <97320296+NickBzy@users.noreply.github.com>
2025-04-15 18:15:45 -0400

    Add carousel code

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -9,3 +9,64 @@ dropdownBtn.addEventListener("click", function () {
     dropdownMenu.classList.add("open")
   }
 })
+
+// CAROUSEL
+let slideIndex = 0
+displaySlides(slideIndex)
+nextSlide()
+previousSlide()
+
+function nextSlide() {
+  const nextBtn = document.querySelector(".next-slide")
+  nextBtn.addEventListener("click", function () {
+    slideIndex += 1
+    displaySlides(slideIndex)
+  })
+}
+
+function previousSlide() {
+  const prevBtn = document.querySelector(".previous-slide")
+  prevBtn.addEventListener("click", function () {
+    slideIndex -= 1
+    displaySlides(slideIndex)
+  })
+}
+
+function displaySlides(slide) {
+  const slides = document.querySelectorAll(".slides")
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].classList.add("hidden")
+  }
+  slideIndex = slide
+  if (slide > slides.length - 1) {
+    slideIndex = 0
+  }
+  if (slide < 0) {
+    slideIndex = slides.length - 1
+  }
+  slides[slideIndex].classList.remove("hidden")
+}
+
+// const wrapper = document.querySelector(".slides-wrapper")
+// const totalSlides = document.querySelectorAll(".slides").length
+// const nextBtn = document.querySelector(".next-slide")
+// const prevBtn = document.querySelector(".previous-slide")
+
+// let currentSlide = 0
+
+// function showSlide(index) {
+//   wrapper.style.transform = `translateX(-${index * 100}%)`
+// }
+
+// nextBtn.addEventListener("click", () => {
+//   currentSlide = (currentSlide + 1) % totalSlides
+//   showSlide(currentSlide)
+// })
+
+// prevBtn.addEventListener("click", () => {
+//   currentSlide = (currentSlide - 1 + totalSlides) % totalSlides
+//   showSlide(currentSlide)
+// })
+
+// // Initial position
+// showSlide(currentSlide)
